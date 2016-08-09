495fa70948d336c1224129c58bb6582a55b2f425
s15002 <s15002@std.it-college.ac.jp>
2016-08-09 18:24:30 +0900

    Add.ログイン画面以外のActivity

---
+++ Application/app/src/main/java/com/example/harapekoyuki/application/OrderDelete.java
@@ -0,0 +1,13 @@
+package com.example.harapekoyuki.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderDelete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_delete);
+    }
+}
